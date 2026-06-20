7dc2e3f2ee5f4bd4bbe5183c803741ed01767799
Joe Mwangi <mwangijoe455@gmail.com>
2026-06-20 09:22:47 +0300

    primitive datatypes in JS

---
+++ JS/intro/PrimitiveData.js
@@ -0,0 +1,51 @@
+/*numbers.
+floating point number ie decimal place
+positive number
+negative number
+*/
+
+let num1=2     //this is a real number
+console.log(num1)    
+let num2=34.45  //floating number
+console.log(num2)
+let num3= -56   // negative number
+console.log(num3)
+
+//strings
+//denoted by " "
+
+let str1= "i am Joe"
+console.log(str1)
+let str2= 'i am joe'
+console.log(str2)
+let str3=`i am joe`
+console.log(str3)
+let str5=`23`
+console.log(str3)
+
+/* why we would use multiple ways of rep a string? */
+let str4= 'joe said "0" is a number'
+
+/*booleans-------> true/false statements*/
+
+let joeIsMale=true
+console.log(joeIsMale)
+let joeIsMarried=false
+console.log(joeIsMarried)
+
+
+/* Null--->lack of a value ie absence of a value*
+what is the difference between undefined and null
+
+never explicitly set sth to undefined 
+if you want to create a variable that does not have a value never use undefined use null*/
+
+let undefinedVal1=undefined  // dont do this mistake
+console.log(undefinedVal1)
+let undefinedVal2;           // js engine set to undefined
+console.log(undefinedVal2)   // undefined
+
+let nullVal1=null
+console.log(nullVal1)
+
+
