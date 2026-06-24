8c309a72ea234a84a84daf0404198c3d5b3a331d
Joe Mwangi <mwangijoe455@gmail.com>
2026-06-25 00:41:49 +0300

    if statements; prompt & alert keywords

---
+++ JS/intro/ifOnly.js
@@ -0,0 +1,26 @@
+/*if statetements
+
+if statements are independent
+*/
+
+let age=prompt('enter your age')
+alert('you entered${age}')
+
+if(age<10){
+    alert('you are a baby')
+}
+
+if(age>13){
+    alert('you are a teen')
+}
+
+if(age>18){
+    alert("you're an adult")
+}
+
+if(age>27){
+    alert('ready for marriage')
+}
+if (age>50){
+    alert('retirement gaining momentum')
+}
