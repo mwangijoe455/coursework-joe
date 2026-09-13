d456dfbbd644551674f17e01caf3f2c5fccdcd5f
Joe Mwangi <mwangijoe455@gmail.com>
2026-09-13 10:49:44 +0300

    intro to python programming

---
+++ Python_course/Python/Intro/condition_and_loop.py
@@ -0,0 +1,44 @@
+# if conditions
+
+if age>18:
+    print('You can drink')
+    print('You can drink sth else')
+    if age=23:
+        print('This is awesome')
+    print('Another line')
+#else if
+elif age<44:
+print('You are not that young')
+else:
+    print('last else')
+
+# for(let i=0;i<100;i++)
+# (start, stop, step)
+for i in range(0,200,2):
+    print('I is', i)
+
+ar=[23,45,67,45, 40]
+for i in the range(0,len(ar)):
+    single_item=ar[i]
+    print(single_item)
+
+#for (let singleItem of arr)
+for single_item in ar:
+    print('single item', single_item)
+
+k=0
+while  k<30:
+    k=k+1
+    print('K is ',k)
+
+#if (age >80){}
+
+if age>80:
+    pass
+
+
+
+
+
+
+
